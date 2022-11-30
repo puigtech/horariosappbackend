39359d6874546f120f8af57eaf19df755b447f26
Guillermo Puig <gpuig97@gmail.com>
2022-11-30 13:50:06 -0500

    Relación de tablas con area

--- src/api/configuracion-horas-extra/routes/configuracion-horas-extra.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * configuracion-horas-extra router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::configuracion-horas-extra.configuracion-horas-extra');
--- src/api/configuracion-horas-extra/services/configuracion-horas-extra.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * configuracion-horas-extra service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::configuracion-horas-extra.configuracion-horas-extra');
